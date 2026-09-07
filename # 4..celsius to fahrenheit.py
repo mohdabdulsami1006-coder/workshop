4002c05bf2941a243d7b5fe97af454299aef569a
mohdabdulsami1006-coder <mohdabdulsami.1006@gmail.com>
2026-09-07 14:54:15 +0530

    Add files via upload

---
+++ # 4..celsius to fahrenheit.py
@@ -0,0 +1,9 @@
+# celsius to fahrenheit
+celsius = float(input("enter temepraturein celsius: "))
+to_fahrenheit = (celsius * 9/5) + 32
+print("temperature in fahrenheit is: ", to_fahrenheit)
+
+# fahrenheit to celsius
+fahrenheit = float(input("enter temperature in fahrenheit: "))
+to_celsius = (fahrenheit - 32) * 5/9
+print("temperature in celsius is: ", to_celsius)

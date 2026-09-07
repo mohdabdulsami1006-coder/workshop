5315cb4ac9510060077e52b998217006c775aa3a
mohdabdulsami1006-coder <mohdabdulsami.1006@gmail.com>
2026-09-07 15:05:26 +0530

    Add files via upload

---
+++ #1.py
@@ -0,0 +1,9 @@
+python
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)
+print("Quotient =", a / b)
+print("Remainder =", a % b
